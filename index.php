061da16a6b65efe8eeae8334bdb2e5d1a85d72bd
Muzakkir212121 <muzakkir.abbas21@gmail.com>
2023-08-21 21:24:35 +0700

    Menambahkan Dashboard

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ switch (ENVIRONMENT)
 {
 	case 'development':
 		error_reporting(-1);
-		ini_set('display_errors', 1);
+		ini_set('error_reporting', E_ALL & ~E_DEPRECATED);
 	break;
 
 	case 'testing':
