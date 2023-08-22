65d445f50a629795bf32c99036d5c40ed1dd6eed
Muzakkir212121 <muzakkir.abbas21@gmail.com>
2023-08-22 19:57:00 +0700

    Melakukan Konfigurasi Templates

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,7 +2,10 @@
 
 class Dashboard extends CI_Controller{
     public function index(){
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
         $this->load->view('dashboard');
+        $this->load->view('templates/footer');
     }
 }
 ?>
