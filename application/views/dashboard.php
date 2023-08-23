259d5168c931dee6188f6fb284e31a5c1a37e5a1
Muzakkir212121 <muzakkir.abbas21@gmail.com>
2023-08-23 21:45:18 +0700

    Menampilkan Produk

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,9 +1,31 @@
 <div class="container-fluid">
-    <div class="row text-center">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="<?php echo base_url('assets/img/slider1.jpg')?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="<?php echo base_url('assets/img/slider2.jpg')?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+</div>
+    <div class="row text-center mt-4">
 
         <?php foreach ($barang as $brg) : ?>
                 
-            <div class="card" style="width: 16rem;">
+            <div class="card ml-3" style="width: 16rem;">
                 <img src="<?php echo base_url(). '/uploads/' .$brg->gambar ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title mb-1"><?php echo $brg->nama_brg?></h5><br />
